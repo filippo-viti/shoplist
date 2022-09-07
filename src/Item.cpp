32b2178bfc919d00bea8a9cfc7c40dbf991b40db
Filippo Viti <filippo.viti2002@gmail.com>
2022-09-07 18:30:05 +0200

    Created Item class. Represents a single list entry

--- src/Item.cpp
+++ src/Item.cpp
@@ -1,5 +1,56 @@
-//
-// Created by filip on 06/09/2022.
-//
-
 #include "Item.h"
+
+Item::Item(const std::string &name, const std::string &description, int quantity, Category category,
+           User *addedBy) : name(name), description(description), quantity(quantity), category(category),
+                            addedBy((User *) addedBy) {}
+
+Item::~Item() {
+}
+
+const std::string &Item::getName() const {
+    return name;
+}
+
+void Item::setName(const std::string &name) {
+    Item::name = name;
+}
+
+const std::string &Item::getDescription() const {
+    return description;
+}
+
+void Item::setDescription(const std::string &description) {
+    Item::description = description;
+}
+
+int Item::getQuantity() const {
+    return quantity;
+}
+
+void Item::setQuantity(int quantity) {
+    Item::quantity = quantity;
+}
+
+Category Item::getCategory() const {
+    return category;
+}
+
+void Item::setCategory(Category category) {
+    Item::category = category;
+}
+
+const User &Item::getAddedBy() const {
+    return addedBy;
+}
+
+void Item::setAddedBy(const User &addedBy) {
+    Item::addedBy = addedBy;
+}
+
+bool Item::operator==(const Item &rhs) const {
+    return name == rhs.name;
+}
+
+bool Item::operator!=(const Item &rhs) const {
+    return !(rhs == *this);
+}
--- src/Item.h
+++ src/Item.h
@@ -1,12 +1,57 @@
-//
-// Created by filip on 06/09/2022.
-//
-
 #ifndef SHOPLIST_ITEM_H
 #define SHOPLIST_ITEM_H
 
 
+#include <string>
+#include "User.h"
+
+enum Category {
+    Dairy,
+    Meat,
+    Vegetables,
+    Fruit,
+    Cleaning,
+    Electronics,
+    Stationery
+};
+
 class Item {
+
+public:
+    Item(const std::string &name, const std::string &description, int quantity, Category category, User *addedBy);
+
+    virtual ~Item();
+
+    const std::string &getName() const;
+
+    void setName(const std::string &name);
+
+    const std::string &getDescription() const;
+
+    void setDescription(const std::string &description);
+
+    int getQuantity() const;
+
+    void setQuantity(int quantity);
+
+    Category getCategory() const;
+
+    void setCategory(Category category);
+
+    const User &getAddedBy() const;
+
+    void setAddedBy(const User &addedBy);
+
+    bool operator==(const Item &rhs) const;
+
+    bool operator!=(const Item &rhs) const;
+
+private:
+    std::string name;
+    std::string description;
+    int quantity;
+    Category category;
+    User *addedBy;
 
 };
 
