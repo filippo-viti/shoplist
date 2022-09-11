06ce2484fdae4feb788455ac6c9052accf82e28b
Filippo Viti <filippo.viti2002@gmail.com>
2022-09-11 17:13:19 +0200

    Implemented methods

--- src/ShoppingList.cpp
+++ src/ShoppingList.cpp
@@ -1,5 +1,41 @@
 #include "ShoppingList.h"
 
+ShoppingList::ShoppingList(User *o) : owner(o) {}
+
+ShoppingList::~ShoppingList() = default;
+
+void ShoppingList::addItem(Item *item) {
+    items.push_back(item);
+}
+
+void ShoppingList::removeItem(Item *item) {
+    items.remove(item);
+}
+
+void ShoppingList::checkItem(Item *item) {
+    //TODO implement method
+}
+
+unsigned int ShoppingList::getBoughtTotal() {
+    return items.size() - missingItems;
+}
+
+unsigned int ShoppingList::getMissingItems() const {
+    return missingItems;
+}
+
+void ShoppingList::setMissingItems(unsigned int missingItems) {
+    ShoppingList::missingItems = missingItems;
+}
+
+User *ShoppingList::getOwner() const {
+    return owner;
+}
+
+void ShoppingList::setOwner(User *owner) {
+    ShoppingList::owner = owner;
+}
+
 void ShoppingList::subscribe(Observer *o) {
     observers.push_back(o);
 }
@@ -14,4 +50,3 @@ void ShoppingList::notify() const {
         o->update();
     }
 }
-
