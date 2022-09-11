4c9e7af87aa4f55413d3ba7304955d52bacae745
Filippo Viti <filippo.viti2002@gmail.com>
2022-09-11 17:31:21 +0200

    Implemented constructor, getters and setters

--- src/User.cpp
+++ src/User.cpp
@@ -1,5 +1,15 @@
 #include "User.h"
 
+User::User(const string &username) : username(username) {}
+
+const string &User::getUsername() const {
+    return username;
+}
+
+void User::setUsername(const string &username) {
+    User::username = username;
+}
+
 void User::update() {
 
 }
