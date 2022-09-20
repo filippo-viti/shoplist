00b9633f21af3660f409118ba63b951bc8691bd9
Filippo Viti <filippo.viti2002@gmail.com>
2022-09-20 15:40:47 +0200

    Implemented main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include "src/User.h"
+#include "src/ShoppingListViewer.h"
+#include "src/MissingItemsBadge.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    ShoppingList shoppingList("Shopping List", new User("Giovanni Storti"));
+
+    ShoppingListViewer listViewer(&shoppingList);
+    MissingItemsBadge missingItemsBadge(&shoppingList);
+
+    Item item1("milk", "dairy");
+    shoppingList.addItem(item1);
+    shoppingList.checkItem("milk");
+    shoppingList.uncheckItem("milk");
+    shoppingList.removeItem("milk");
 }
